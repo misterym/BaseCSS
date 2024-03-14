60f46390bb806284204501884f8e5cb8b91eb71e
misterym <145797705+misterym@users.noreply.github.com>
2024-03-14 15:55:40 -0300

    Create bcss.cc

---
+++ bcss.cc
@@ -0,0 +1,295 @@
+#left{height:0px}
+a:hover {text-decoration: none!important;}
+div#main-content > div[style="overflow:visible"] {display: none;}
+.sceditor-container .sceditor-button {border: var(--border)!important;}
+.sceditor-button.active, .sceditor-button:active, .sceditor-button:hover {
+    background: var(--accent)!important;
+    color: var(--background-1)!important;
+}
+#s-m-t-tooltip {
+    max-width: 350px;
+    z-index: 10;
+    margin: 24px 14px 7px 12px;
+    padding: 8px;
+    background: var(--background-1);
+    font-family: var(--font2);
+    font-size: 12px;
+    line-height: 16px;
+    color: var(--color);
+    border: var(--border);
+    text-transform:uppercase; font-weight:600;
+} 
+select {
+    border: var(--border);
+    background: var(--background-1);
+    color: var(--color1);
+    font-size: 12px;
+    padding: 2px 10px;
+}
+#page-footer p.copyright strong {
+    margin: 0;
+    display: block;
+}
+
+.copyright strong {
+    text-align: center;
+    background: var(--background-1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 0;
+    line-height: 10px;
+    font-size: .7rem;
+    margin-block: 0;
+    text-transform: uppercase;
+    padding: 15px;
+    border: var(--border);
+}
+#page-footer .navbar {
+    margin-bottom: 50px;
+    border: var(--border);
+    font-size: .6rem;
+    color: var(--color1);
+    background: var(--background-2);
+    text-transform: uppercase;
+    font-weight: 500;
+    letter-spacing: .5px;
+    padding: 5px 10px;
+}
+.navbar li strong, .navbar li a {
+    font-size: 10px;
+    padding: 1px;
+    font-weight: 600 !important;
+    opacity: .65;
+}
+
+input[type="checkbox" i] {
+    accent-color: var(--accent-1);
+    background-color: field;
+    border: var(--border);
+}
+.chatbox-title, .chatbox-title a.chat-title {
+    color: #!important;
+    font-family: var(--font-1);
+}
+a, a:visited, a:active, a:focus{color:var(--accent-1);}
+::selection{background:var(--accent-1); color:#fafafa;}
+#fa_toolbar{display:none;}
+div#main {overflow: visible!important;}
+::-webkit-scrollbar {
+    width: 8px !important;
+    height: 5px !important;
+    background: var(--background-1);
+    border-left: var(--border);
+}
+.jqmWindow h1 {
+    font-family: var(--font-2);
+    font-weight: 900;
+    color: var(--accent-1);
+    border-bottom: var(--border);
+    padding: 10px;
+}
+.jqmWindow, #find_username {
+    background: var(--background-1);
+    border: var(--border);
+    color: var(--color);
+    border-radius: 10px;
+    padding: 10px;
+}
+::-webkit-scrollbar-thumb {
+    background: linear-gradient( 40deg, var(--gradient1),var(--gradient2));
+}
+input {
+    background: var(--background-1);
+    border: var(--border)!important;
+    border-radius: 4px;
+    color: var(--color1);
+}
+span.corners-bottom, span.corners-top {display: none;}
+.conteneur_minwidth_IE:after {
+    content: "";
+    height: 20px;
+    width: 100%;
+    display: block;
+}
+input:focus-visible {
+    outline: 1px solid var(--accent-2);
+}
+#tabs a span {
+    padding: 2px!important;
+    font-weight:700px;
+}
+#cp-main {
+    border: var(--border);
+    border-color: var(--accent-1);
+    margin: 10px;
+}
+dt label{font-weight:400;}
+fieldset.submit-buttons {
+    padding: 10px;
+    background: var(--accent-2);
+    margin: 0;
+}
+.panel > .inner > fieldset > dl > dt, fieldset dl:hover dt label {
+    color: unset!important;
+    font-family: var(--font-2);
+    font-size: .8rem;
+}
+.pagination span a, .pagination span a:link, .pagination span a:visited, .pagination span a:active {
+    display: inline-flex;
+    width: 25px;
+    height: 25px;
+    border: var(--border);
+    align-items: center;
+    justify-content: center;
+    background: var(--background-1);
+    border-radius: 3px;
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--accent-1);
+}
+ .pagination span strong, .pagination span a:link:not(.pag-img) {
+    display: inline-flex;
+    width: 25px;
+    height: 25px;
+    border: var(--border);
+    align-items: center;
+    justify-content: center;
+    background: var(--background-1);
+    border-radius: 3px;
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--accent-1);
+}
+.pagination > span > strong {
+    color: var(--color-icons);
+    font-weight: 800;
+}
+p.pagination {
+    margin-bottom: 0;
+    font-size: 0;
+}
+#content-container div#main {
+    margin-bottom: 0;
+}
+#page-footer p.copyright strong:empty{display:none;}
+#tabs {
+    line-height: normal;
+    margin: 0px;
+    border: var(--border);
+    padding: 15px;
+    border-bottom: 0;
+    background: var(--background-1);
+    margin-bottom: 0px;
+}
+div#tabs > ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 5px;
+}
+#tabs li {
+    flex-grow: 1;
+    border: var(--border);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px 20px;
+    background: var(--background-1);
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: .5px;
+    font-size: .65rem;
+}
+li.activetab a {
+    color: var(--color1);
+    font-weight: 400;
+    letter-spacing: 2px;
+    opacity: .4;
+}
+.panel.row3 {
+    border: var(--border);
+    background: var(--background-1);
+    margin-top: 0px;
+    padding: 0;
+}
+h1.page-title {
+    margin: 0!important;
+    font-family: var(--font-1);
+    font-weight: 500;
+    color: #fafafa;
+    padding: 10px!important;
+    background: var(--accent-2);
+    border-bottom: 1px solid var(--accent-1)!important;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-size: .85rem;
+}
+.panel > .inner > fieldset > dl > dd > input {
+    margin-bottom: 15px;
+    margin-left: 10px;
+    padding: 4px 10px;
+}
+h1.page-tit {
+    background: var(--background-1);
+    border-bottom: var(--border);
+    font-family: var(--font-1);
+    font-size: 24px;
+    margin: -22px 11px 50px -15px;
+    padding: 40px 30px;
+    width: 1070px;
+}
+.panel > .inner > fieldset > dl {
+    border: var(--border);
+    margin: 10px 0;
+    background: var(--background-2);
+    padding: 15px;
+    font-size: .8rem;
+}
+fieldset dl {
+    padding: 4px 0;
+}
+#cp-main .panel:not(:last-child) {
+    border-bottom: 1px solid var(--accent-1);
+    margin-bottom: 0;
+}
+
+#cp-main .panel {
+    padding: 20px;
+    font-size: .74rem;
+    color: var(--color);
+}
+#tabs {
+    line-height: normal;
+    margin: 0px;
+    border: var(--border);
+    padding: 15px;
+    border-bottom: 0;
+    background: var(--background-1);
+    margin-bottom: 0px;
+}
+#tabs .activetab a span {
+    color: #fafafa !important;
+}
+#tabs .activetab {
+    background: var(--accent-2);
+    opacity: 1;
+}
+.inputbox {
+    border: var(--border);
+    background: var(--background-1);
+}
+p.pagination > a[href="javascript:Pagination();"], .pag-img, a.pag-img{
+    display: none!important;
+}
+html#sceditor_smilies ::-webkit-scrollbar {
+    width: 8px !important;
+    height: 5px !important;
+    background: var(--background-1);
+    border-left: var(--border);
+}
+
+html#sceditor_smilies:: -webkit-scrollbar-thumb {
+    background: linear-gradient(40deg, var(--gradient1), var(--gradient2));
+}
